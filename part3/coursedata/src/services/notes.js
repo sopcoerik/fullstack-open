c69fda225c4bc429829efe19f67b836ed26501d3
sopcoerik <sopcoerik@gmail.com>
2023-11-01 10:08:50 +0200

    coursedata-backend

--- part3/coursedata/src/services/notes.js
+++ part3/coursedata/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
@@ -16,6 +16,4 @@ const update = (id, newObject) => {
   return request.then(response => response.data)
 }
 
-export default { 
-  getAll, create, update 
-}
+export default { getAll, create, update }
